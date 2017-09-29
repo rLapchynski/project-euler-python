3a7c2ca9c52d2d64449b411d1ccd4a445aac9235
Ryan Lapchynski <rlapchynski8@gmail.com>
2017-09-29 14:07:16 -0400

    Solved problem 1

---
+++ problem1.py
@@ -0,0 +1,9 @@
+sum = 0
+# add all multiples of 3 to sum
+for i in range(0, 1000, 3):
+    sum += i
+# add all multiples of 5 to sum, if they're not a multiple of 3. (so 15, 30, 45, etc. aren't duplicated)
+for i in range(0, 1000, 5):
+    if i%3 != 0:
+        sum += i
+print(sum)
