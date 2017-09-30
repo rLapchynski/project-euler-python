19c5461e7976f8a619425b42bbb8cba9c5cfd9fc
Ryan Lapchynski <rlapchynski8@gmail.com>
2017-09-30 00:29:12 -0400

    Solved problem 3, created utilities.py, and several functions therein

---
+++ problem3.py
@@ -0,0 +1,5 @@
+# https://projecteuler.net/problem=3
+
+from utilities import *
+
+print(factors(600851475143)[-1])
---
+++ utilities.py
@@ -0,0 +1,106 @@
+##
+## Contains functions for basic math operations that will be reused
+##
+
+from math import *
+
+def isPrime(num, negativePrime=True):
+    """Determines is a number is prime
+
+       If negativePrime==True (default: True), then negative numbers can be prime
+    """
+    if negativePrime:
+        num = abs(num)
+
+    a = factors(num, True)
+    return a != [] and a[0] == num
+
+def factors(num, primalityTest=False):
+    """Determines the prime factors of a number
+
+       If primalityTest==True (default: False), it will return when it finds the first non-trivial factor.
+       Uses trial division for num<10e4, Pollard's rho algorithm for num<10e10, quadratic sieve for num<10e100, and general number field sieve for num>10e100
+    """
+    if num <= 10e4:
+        return trialFactors(num, primalityTest)
+    elif num <= 10e10:
+        #TODO Change to rhoFactors
+        return trialFactors(num, primalityTest)
+    elif num <= 10e100:
+        #TODO Change to qsFactors
+        return trialFactors(num, primalityTest)
+    else:
+        #TODO Change to gnfsFactors
+        return trialFactors(num, primalityTest)
+
+def trialFactors(num, primalityTest=False, includeNeg=True):
+    """Determines the prime factors of a number by trial division.
+
+       If primalityTest==True (default: False), it will return when it finds the first non-trivial factor.
+       If includeNeg==True (default: True), it will include -1 as a factor of negative numbers
+    """
+
+    factors = []
+    originalNum = num
+
+    # Beyond 2 and 3, any prime number is +/-1 from a multiple of 6, and only
+    #   prime numbers need to be checked as factors, so only 6n+/-1 for int n
+    #   need to be checked.
+    # Only factors up to sqrt(num) need to be tested, because any factor
+    #   >sqrt(num) must have a corresponding factor <sqrt(num)
+    # Negative numbers are given the factor -1 if includeNeg==True and continue as positive numbers
+    # 0, 1, -1 have no factors and returns an empty list
+
+    if num < -1:
+        if includeNeg:
+            factors.append(-1)
+        num = abs(num)
+    elif num <= 1:
+        return []
+
+    # Remove all factors that are 2 because 2 is not 6n+/-1
+    while num%2==0:
+        factors.append(2)
+        num /= 2
+        if primalityTest:
+            return factors
+
+    # Remove all factors that are 3 because 3 is not 6n+/-1
+    while num%3==0:
+        factors.append(3)
+        num /= 3
+        if primalityTest:
+            return factors
+
+    while num != 1:
+        for i in range(6, int(ceil(sqrt(num)))+6, 6):
+            if num % (i-1) == 0:
+                factors.append(i-1)
+                if primalityTest:
+                    return factors
+                num /= i-1
+                break
+            if num % (i+1) == 0:
+                factors.append(i+1)
+                if primalityTest:
+                    return factors
+                num /= i+1
+                break
+        else:
+            # neither of the above breaks executed for any prime i+/-1<sqrt(num) so num is prime
+            factors.append(int(num))
+            break
+
+    return factors
+
+def rhoFactors(num, primalityTest=False):
+    #TODO
+    pass
+
+def qsFactors(num, primalityTest=False):
+    #TODO
+    pass
+
+def gnfsFactors(num, primalityTest=False):
+    #TODO
+    pass
